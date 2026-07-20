a3736bb13de51145cdbdb6577ac811a55c2da78f
Ryan <ryandejonge@outlook.com>
2026-07-20 18:43:57 +0200

    initial project

---
+++ Dev.Naamloos.Fennec/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dev.Naamloos.Fennec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
